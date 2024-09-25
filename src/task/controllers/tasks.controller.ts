655f3130eee244f14a8550c51f6773743c964b13
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2024-09-24 20:47:54 -0600

    feat: remove unused imports

--- src/task/controllers/tasks.controller.ts
+++ src/task/controllers/tasks.controller.ts
@@ -17,7 +17,6 @@ import { DeleteTaskCommand } from '../cqrs/commands/delete-task.command';
 import { GetTasksQuery } from '../cqrs/queries/get-tasks.query';
 import { GetTaskByIdQuery } from '../cqrs/queries/get-task-by-id.query';
 import { GetTasksQueryParamsDto } from '../dtos/get-tasks-query-params.dto';
-import { IsInt } from 'class-validator';
 import { FindOneParamsDto } from '../dtos/find-one-params.dto';
 
 @Controller('tasks')
